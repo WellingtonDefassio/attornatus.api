560784cd9b14fd19e4dd677386ee37798dbada23
Wellington Defassio <well.defassio@gmail.com>
2023-01-05 22:12:18 -0300

    criado metodo para validação de formato data.

--- src/main/java/com/example/attornatus/api/util/DateUtil.java
+++ src/main/java/com/example/attornatus/api/util/DateUtil.java
@@ -1,18 +1,29 @@
 package com.example.attornatus.api.util;
 
+import com.example.attornatus.api.exception.IncorrectDateFormat;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class DateUtil {
 
-
+  private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
     /**
      * @param date recive a LocalDate
      * @return a String with dd-MM-yyyy format
      */
     public static String stringFormat(LocalDate date) {
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
         return date.format(dateTimeFormatter);
+    }
+
+    public static LocalDate stringToDate(String data) {
+
+        try {
+          return LocalDate.parse(data,dateTimeFormatter);
+        } catch (Exception e) {
+            throw new IncorrectDateFormat("o formato da data deve ser dd-mm-yyyy");
+        }
     }
 
 
